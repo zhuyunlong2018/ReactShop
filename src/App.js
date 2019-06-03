80eb254e4bdd8be409b462cf597bad999024e3b4
ZhuYunlong <920200256@qq.com>
2019-06-03 11:35:01 +0800

    添加react-router-cache-route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CacheRoute, CacheSwitch } from 'react-router-cache-route'
 import store from 'SRC/store'
 import Pages from 'SRC/pages/Pages.jsx'
 import Login from 'SRC/pages/login/Login'
@@ -18,14 +19,12 @@ class App extends Component {
     basename: process.env.REACT_APP_BASENAME
   };
 
-
   componentWillMount() {
     //读取本地存储是否有用户信息
     const userInfo = Storage.getStorage(USER_KEY)
     if (userInfo) {
       store.dispatch(saveInfo(userInfo))
     }
-    console.log(this.state.basename)
   }
 
   render() {
@@ -33,10 +32,12 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter basename={this.state.basename}>
           <div className="App">
+            <CacheSwitch>
+              <CacheRoute path='/' exact component={Pages}></CacheRoute>
+              <CacheRoute path='/search' exact component={Search}></CacheRoute>
+            </CacheSwitch>
             <Switch>
-              <Route path='/' exact component={Pages}></Route>
               <Route path='/login' exact component={Login}></Route>
-              <Route path='/search' exact component={Search}></Route>
             </Switch>
           </div>
         </BrowserRouter>
--- src/pages/home/components/HomeBannel.js
+++ src/pages/home/components/HomeBannel.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { getBannel } from 'SRC/api/home'
 import HomeBannelUI from './HomeBannelUI'
-
+import { withRouter } from 'react-router-dom'
 class HomeBannel extends React.Component {
+
+  constructor(props, ...args) {
+    super(props, ...args)
+    props.cacheLifecycles.didCache(this.componentDidCache)
+    props.cacheLifecycles.didRecover(this.componentDidRecover)
+  }
+
   state = {
     data: [],
     imgHeight: 176,
@@ -16,9 +23,15 @@ class HomeBannel extends React.Component {
     }).catch(error => { })
   }
 
+  componentDidRecover = () => {
+    // 强制更新
+    this.forceUpdate();
+  }
+
   componentDidMount() {
     this.getBannel()
   }
+
   render() {
     return (
       <HomeBannelUI data={this.state.data}
@@ -27,4 +40,4 @@ class HomeBannel extends React.Component {
   }
 }
 
-export default HomeBannel;
+export default withRouter(HomeBannel);
